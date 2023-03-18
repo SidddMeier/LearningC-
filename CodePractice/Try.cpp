e8fa5e75ed1d86f2e3b997c5890aa9bd738937b5
SidddMeier <qzb200011@163.com>
2023-03-18 15:33:29 +0800

    updata in 3/18

--- CodePractice/Try.cpp
+++ CodePractice/Try.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+#include<iterator>
 using namespace std;
 int main(){
     cout<< "Hello World"<<endl;
     cout << "This is just a try!" << endl;
+    char arr[] = "Hello";
+    char * str = arr;
+    char *begin = str;
+    char *endll = end(arr);
+    cout << endll - begin<<endl << *(--endll) <<endl;
 }
